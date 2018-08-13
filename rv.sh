85859e89ec8667d01bfd48ed14e84cdd57e62552
Yiyi Wang <shd101wyy@sina.com>
2018-08-13 17:28:25 -0500

    .

--- rv.sh
+++ rv.sh
@@ -5,29 +5,26 @@ set -x
 # Install necessary packages. 
 sudo apt-get install -y libreadline-dev
 
-VERSION=5.3.4
-tar_tests=lua-$VERSION-tests.tar.gz
-wget https://www.lua.org/tests/$tar_tests
-tar -xzf $tar_tests
+# VERSION=5.3.4
+# tar_tests=lua-$VERSION-tests.tar.gz
+# wget https://www.lua.org/tests/$tar_tests
+# tar -xzf $tar_tests
 
-json_out=`pwd`/my_errors.json
+error_file=`pwd`/my_errors.json
 compiler=kcc
-reportflag="CFLAGS=-fissue-report=$json_out"
-sudo make -j`nproc` CC=$compiler LD=$compiler $reportflag 
+sudo make -j`nproc` CC=$compiler LD=$compiler CFLAGS=-fissue-report=$error_file 
+rm $error_file
 
 `pwd`/lua -e "print(\"Hello, World!\")"
 
-(
-cd lua-$VERSION-tests/
-../lua sort.lua
-)
-
-ls -la
-wc -l my_errors.json
+# (
+# cd lua-$VERSION-tests/
+# ../lua sort.lua
+# )
 
 # Generate a HTML report with `rv-html-report` command,
 # and output the HTML report to `./report` directory. 
-rv-html-report my_errors.json -o report
+rv-html-report $error_file -o report
 
 # Upload your HTML report to RV-Toolkit website with `rv-upload-report` command. 
 rv-upload-report `pwd`/report
