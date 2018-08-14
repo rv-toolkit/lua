740b4c9d4b03b467bca84936d4374665db9caa70
Yiyi Wang <shd101wyy@sina.com>
2018-08-14 15:34:59 -0500

    Update rv.sh

--- rv.sh
+++ rv.sh
@@ -1,26 +1,17 @@
 #!/bin/sh
-set -x
+set -x # print debug info
 
 # This is running under Ubuntu 16.04
 # Install necessary packages. 
 sudo apt-get install -y libreadline-dev
 
-# VERSION=5.3.4
-# tar_tests=lua-$VERSION-tests.tar.gz
-# wget https://www.lua.org/tests/$tar_tests
-# tar -xzf $tar_tests
-
+# Compile and run `lua`.
 error_file=`pwd`/my_errors.json
 compiler=kcc
 sudo make -j`nproc` CC=$compiler LD=$compiler CFLAGS=-fissue-report=$error_file 
 rm $error_file
 
 `pwd`/lua -e "print(\"Hello, World!\")"
-
-# (
-# cd lua-$VERSION-tests/
-# ../lua sort.lua
-# )
 
 # Generate a HTML report with `rv-html-report` command,
 # and output the HTML report to `./report` directory. 
