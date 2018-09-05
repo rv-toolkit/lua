794f1e0f2ead794d190017d4f567fd302412e8ce
yiyiwang <shd101wyy@sina.com>
2018-09-05 11:07:11 -0500

    Refactor rv.sh

--- rv.sh
+++ rv.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 set -x # print debug info
 
+json_out=`pwd`/errors.json
+report_out=`pwd`/report
+
 # This is running under Ubuntu 16.04
 # Install necessary packages. 
-sudo apt-get install -y libreadline-dev
+apt install -y libreadline-dev
 
 # Compile and run `lua`.
-error_file=`pwd`/my_errors.json
 compiler=kcc
-sudo make -j`nproc` CC=$compiler LD=$compiler CFLAGS=-fissue-report=$error_file 
-rm $error_file
+make -j`nproc` CC=$compiler LD=$compiler CFLAGS="-fissue-report=$json_out" 
+rm $json_out
 
 `pwd`/lua -e "print(\"Hello, World!\")"
 
 # Generate a HTML report with `rv-html-report` command,
 # and output the HTML report to `./report` directory. 
-rv-html-report $error_file -o report
+touch $json_out && rv-html-report $json_out -o $report_out
 
 # Upload your HTML report to RV-Toolkit website with `rv-upload-report` command. 
-rv-upload-report `pwd`/report
+rv-upload-report $report_out
 
 # Done.
